d082b313b865b73fe716702bcffb2cbc018c06c1
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-08-22 23:34:41 +0200

    use ref with createRef()

--- src/BMREditor.js
+++ src/BMREditor.js
@@ -1,30 +1,36 @@
 import React from "react";
 
 export class BMREditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.age = React.createRef();
+        this.height = React.createRef();
+        this.weight = React.createRef();
+    }
 
   handleSubmit = (event) => {
       this.props.onCalculate({
-      age: this.age.value,
-      height: this.height.value,
-      weight: this.weight.value,
+      age: this.age.current.value,
+      height: this.height.current.value,
+      weight: this.weight.current.value,
     });
     event.preventDefault();
-      this.age.value = '';
-      this.height.value = '';
-      this.weight.value = '';
+      this.age.current.value = '';
+      this.height.current.value = '';
+      this.weight.current.valuet = '';
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="BMREditor">
         <label htmlFor="">
-          Age <input type="text" ref={age => this.age = age} />
+          Age <input type="text" ref={this.age} />
         </label>
         <label htmlFor="">
-          Height <input type="text" ref={height => this.height = height} />
+          Height <input type="text" ref={this.height} />
         </label>
         <label htmlFor="">
-          Weight <input type="text" ref={weight => this.weight = weight} />
+          Weight <input type="text" ref={this.weight} />
         </label>
         <button>Calculate</button>
       </form>
